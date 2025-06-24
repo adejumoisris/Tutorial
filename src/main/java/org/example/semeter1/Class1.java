181f86549d39f03564abee068f58f2eaa2644e8b
deca <idrisadejumo@gmail.com>
2025-06-24 09:36:40 +0100

    second commit

---
+++ src/main/java/org/example/semeter1/Class1.java
@@ -0,0 +1,18 @@
+package org.example.semeter1;
+
+public class Class1 {
+ /**
+  * Accesss modifer
+  * 1 Public
+  * 2.private
+  * 3.protected **/
+
+ public   String malik(){
+     return  "Malik is looking at the scren ";
+ }
+    public static void main(String[] args) {
+     Class1 class1 = new Class1();
+
+        System.out.println(class1.malik());
+    }
+}
---
+++ src/main/java/org/example/semeter1/Class2.java
@@ -0,0 +1,11 @@
+package org.example.semeter1;
+
+public class Class2 {
+    public static void main(String[] args) {
+        Class1 class1 = new Class1();
+        System.out.println(class1.malik());
+
+    }
+
+
+}
