8f5e4df229bd368fc6fad3974ece3e631166d5f2
deca <idrisadejumo@gmail.com>
2025-06-25 10:16:49 +0100

    staff

---
+++ src/main/java/org/example/learn/Chisom.java
@@ -0,0 +1,12 @@
+package org.example.learn;
+
+public class Chisom {
+
+    public static int chisom (int a , int b ){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(chisom(3,8));
+    }
+}
